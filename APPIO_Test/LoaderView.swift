ca974d61099a9db8c18024be676f7530b80f6396
Luvseji <mr.rtte@icloud.com>
2022-01-27 23:55:37 +0700

    Добавление анимаций на загрузочный лоадер, настройка стартового VC

--- APPIO_Test/LoaderView.swift
+++ APPIO_Test/LoaderView.swift
@@ -9,17 +9,22 @@ import UIKit
 
 class LoaderView: UIView {
     
+    let outterSize = 100.0
+    let outterBackground = CAGradientLayer()
     private lazy var outterView: UIView = {
         let color = #colorLiteral(red: 0.3882352941, green: 0.5843137255, blue: 1, alpha: 1)
         return createLoaderSubview(backgroundColor: color,
                                    borderWidth: 1)
     }()
     
+    let innerSize = 70.0
+    let innerBorder = CAGradientLayer()
     private lazy var innerView: UIView = {
         return createLoaderSubview(backgroundColor: UIColor.white,
                                    borderWidth: 1)
     }()
     
+    let pointSize = 13.5
     private lazy var pointView: UIView = {
         return createLoaderSubview(backgroundColor: UIColor.black,
                                    borderWidth: 0.5)
@@ -32,36 +37,140 @@ class LoaderView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.outterView.layer.cornerRadius = self.outterView.frame.width / 2.0
-        self.innerView.layer.cornerRadius = self.innerView.frame.width / 2.0
-        self.pointView.layer.cornerRadius = self.pointView.frame.width / 2.0
+        
+        setGradientBackground(for: outterView)
+        setGradientBorder(for: innerView)
+        
+        outterView.layer.cornerRadius = outterView.frame.width / 2.0
+        innerView.layer.cornerRadius = innerView.frame.width / 2.0
+        pointView.layer.cornerRadius = pointView.frame.width / 2.0
 
-        self.pointView.center = self.getPoint(for: -90)
+        pointView.center = getPoint(for: -90)
     }
     
     func startAnimating() {
-        let path = UIBezierPath()
-        let initialPoint = self.getPoint(for: -90)
-        path.move(to: initialPoint)
-        for angle in -89...0 { path.addLine(to: self.getPoint(for: angle)) }
-        for angle in 1...270 { path.addLine(to: self.getPoint(for: angle)) }
-        path.close()
-        self.animate(view: self.pointView, path: path)
+        animatePointPosition()
+        animateBackgroundColorFirst {
+            self.outterBackground.colors = [
+                #colorLiteral(red: 1, green: 0.001965026837, blue: 0.002425886225, alpha: 1).cgColor,
+                #colorLiteral(red: 0.5440160632, green: 0.06818716228, blue: 0.9537369609, alpha: 1).cgColor
+            ]
+            self.animateBackgroundColorContinue()
+        }
+        animateSize()
+        animateBorderOpacity()
+        animateBorderWidth()
     }
     
 }
 
 private extension LoaderView {
     
-    private func animate(view: UIView, path: UIBezierPath) {
+    func animateBorderWidth() {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat, .autoreverse]) {
+            self.outterView.layer.borderWidth = 0
+            self.innerView.layer.borderWidth = 0
+        }
+    }
+    
+    func animateBorderOpacity() {
+        let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+        opacityAnimation.duration = 2.0
+        opacityAnimation.fromValue = 0
+        opacityAnimation.toValue = 1
+        opacityAnimation.repeatCount = .infinity
+        opacityAnimation.autoreverses = true
+        innerBorder.add(opacityAnimation, forKey: "opacityAnimation")
+    }
+    
+    func animateSize() {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat, .autoreverse]) {
+            self.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }
+
+    }
+    
+    func animateBackgroundColorFirst(completion: @escaping (() -> Void)) {
+        CATransaction.begin()
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 2.0
+        gradientChangeAnimation.toValue = [
+            #colorLiteral(red: 1, green: 0.001965026837, blue: 0.002425886225, alpha: 1).cgColor,
+            #colorLiteral(red: 0.5440160632, green: 0.06818716228, blue: 0.9537369609, alpha: 1).cgColor
+        ]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        CATransaction.setCompletionBlock(completion)
+        outterBackground.add(gradientChangeAnimation, forKey: "backgroundAnimation")
+        CATransaction.commit()
+    }
+    
+    func animateBackgroundColorContinue() {
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 2.0
+        gradientChangeAnimation.toValue = [
+            #colorLiteral(red: 0.5895209908, green: 0.5640896559, blue: 0.9912117124, alpha: 1).cgColor,
+            #colorLiteral(red: 0.9885050654, green: 0.4543109536, blue: 0.4580554962, alpha: 1).cgColor
+        ]
+        gradientChangeAnimation.repeatCount = .infinity
+        gradientChangeAnimation.autoreverses = true
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        outterBackground.add(gradientChangeAnimation, forKey: "backgroundAnimation")
+    }
+    
+    func animatePointPosition() {
+        let path = UIBezierPath()
+        let initialPoint = self.getPoint(for: -90)
+        path.move(to: initialPoint)
+        for angle in -89...0 { path.addLine(to: self.getPoint(for: angle)) }
+        for angle in 1...270 { path.addLine(to: self.getPoint(for: angle)) }
+        path.close()
+        
         let animation = CAKeyframeAnimation(keyPath: "position")
         animation.path = path.cgPath
         animation.repeatCount = .infinity
-        animation.duration = 5
-        view.layer.add(animation, forKey: "circleAnimation")
-      }
+        animation.duration = 1
+        pointView.layer.add(animation, forKey: "circleAnimation")
+    }
     
-    private func getPoint(for angle: Int) -> CGPoint {
+    func setGradientBorder(for view: UIView) {
+        innerBorder.frame =  CGRect(origin: CGPoint.zero, size: view.frame.size)
+        innerBorder.colors = [
+            #colorLiteral(red: 1, green: 0.7607843137, blue: 0, alpha: 1).cgColor,
+            #colorLiteral(red: 1, green: 0.9490196078, blue: 0.8078431373, alpha: 1).cgColor,
+            #colorLiteral(red: 1, green: 0.6470588235, blue: 0.1176470588, alpha: 1).cgColor,
+            #colorLiteral(red: 0.9725490196, green: 0.8705882353, blue: 0.6666666667, alpha: 1).cgColor,
+            #colorLiteral(red: 0.9960784314, green: 0.8588235294, blue: 0.3294117647, alpha: 1).cgColor
+        ]
+        innerBorder.startPoint = .zero
+        innerBorder.endPoint = CGPoint(x: 1, y: 1)
+        innerBorder.cornerRadius = view.frame.width / 2
+
+        let shape = CAShapeLayer()
+        shape.lineWidth = 15
+        shape.path = UIBezierPath(roundedRect: view.bounds,
+                                             cornerRadius: view.frame.width / 2).cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        innerBorder.mask = shape
+        innerBorder.opacity = 0
+        view.layer.addSublayer(innerBorder)
+    }
+    
+    func setGradientBackground(for view: UIView) {
+        outterBackground.frame = view.bounds
+        outterBackground.colors = [
+            #colorLiteral(red: 0.3882352941, green: 0.5843137255, blue: 1, alpha: 1).cgColor,
+            #colorLiteral(red: 0.3882352941, green: 0.5843137255, blue: 1, alpha: 1).cgColor
+        ]
+        outterBackground.startPoint = CGPoint(x:0, y:0)
+        outterBackground.endPoint = CGPoint(x:1, y:1)
+        outterBackground.cornerRadius = view.frame.width / 2
+        view.layer.addSublayer(outterBackground)
+    }
+    
+    func getPoint(for angle: Int) -> CGPoint {
         let radius = Double(outterView.frame.width / 2)
         let radian = Double(angle) * Double.pi / Double(180)
         let pointCoeff = radius - pointView.frame.width / 2.0 - outterView.layer.borderWidth
@@ -76,24 +185,24 @@ private extension LoaderView {
         
         self.addSubview(self.outterView)
         NSLayoutConstraint.activate([
-            self.outterView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.outterView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.outterView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.outterView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            self.outterView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.outterView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.outterView.widthAnchor.constraint(equalToConstant: outterSize),
+            self.outterView.heightAnchor.constraint(equalToConstant: outterSize)
         ])
         
         self.addSubview(self.innerView)
         NSLayoutConstraint.activate([
             self.innerView.centerXAnchor.constraint(equalTo: outterView.centerXAnchor),
             self.innerView.centerYAnchor.constraint(equalTo: outterView.centerYAnchor),
-            self.innerView.widthAnchor.constraint(equalToConstant: 70),
-            self.innerView.heightAnchor.constraint(equalToConstant: 70)
+            self.innerView.widthAnchor.constraint(equalToConstant: innerSize),
+            self.innerView.heightAnchor.constraint(equalToConstant: innerSize)
         ])
         
         self.addSubview(self.pointView)
         NSLayoutConstraint.activate([
-            self.pointView.widthAnchor.constraint(equalToConstant: 13.5),
-            self.pointView.heightAnchor.constraint(equalToConstant: 13.5)
+            self.pointView.widthAnchor.constraint(equalToConstant: pointSize),
+            self.pointView.heightAnchor.constraint(equalToConstant: pointSize)
         ])
     }
     
--- APPIO_Test/ViewController.swift
+++ APPIO_Test/ViewController.swift
@@ -9,7 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var loaderView: LoaderView!
+    @IBOutlet private weak var titleImage: UIImageView!
+    @IBOutlet private weak var logoImage: UIImageView!
+    @IBOutlet private weak var loaderView: LoaderView!
+    @IBOutlet private weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTransition()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        titleImage.layer.opacity = 0
+        logoImage.layer.opacity = 0
+        startButton.layer.opacity = 0
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -19,6 +36,41 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+    }
+    
 
+}
+
+private extension ViewController {
+    
+    func setupTransition() {
+        let seconds = Double.random(in: 2.0 ... 5.0)
+        let _ = Timer.scheduledTimer(timeInterval: seconds, target: self, selector: #selector(updateAfterLoading), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateAfterLoading() {
+        UIView.animate(withDuration: 1.0, delay: 0.0) {
+            self.loaderView.layer.opacity = 0
+        } completion: { _ in
+            self.loaderView.isHidden = true
+            UIView.animate(withDuration: 0.4, delay: 0.0) {
+                self.titleImage.layer.opacity = 1
+            } completion: { _ in
+                UIView.animate(withDuration: 0.4, delay: 0.0) {
+                    self.logoImage.layer.opacity = 1
+                } completion: { _ in
+                    UIView.animate(withDuration: 0.4, delay: 0.0) {
+                        self.startButton.layer.opacity = 1
+                    }
+                }
+            }
+        }
+
+
+    }
 }
 
